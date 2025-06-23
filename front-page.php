5e0c867194cc4ce484788f2a754ed6192850a171
chouenne <134743005+chouenne@users.noreply.github.com>
2025-06-23 11:16:39 -0400

    Add local project files

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<section class="hero">
+  <div class="hero-content">
+    <h1>Unlock Limitless Art</h1>
+    <p>Join Pro to access premium digital illustrations</p>
+    <a href="#" class="btn-primary">Join Pro</a>
+  </div>
+</section>
+
+<section class="categories">
+  <h2>Featured Categories</h2>
+  <ul class="featured-categories">
+    <li class="category-item">Wall Art</li>
+    <li class="category-item">Line Art</li>
+    <li class="category-item">Posters</li>
+    <li class="category-item">Nature</li>
+  </ul>
+</section>
+
+<?php get_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+
+//Load parent theme styles
+add_action('wp_enqueue_scripts', 'xh_enqueue_styles');
+function xh_enqueue_styles()
+{
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+
+add_action('init', 'xh_remove_storefront_header_actions');
+function xh_remove_storefront_header_actions()
+{
+  remove_action('storefront_header', 'storefront_header_container', 0);
+  remove_action('storefront_header', 'storefront_skip_links', 5);
+  remove_action('storefront_header', 'storefront_social_icons', 10);
+  remove_action('storefront_header', 'storefront_site_branding', 20);
+  remove_action('storefront_header', 'storefront_secondary_navigation', 30);
+  remove_action('storefront_header', 'storefront_product_search', 40);
+  remove_action('storefront_header', 'storefront_header_container_close', 41);
+  remove_action('storefront_header', 'storefront_primary_navigation_wrapper', 42);
+  remove_action('storefront_header', 'storefront_primary_navigation', 50);
+  remove_action('storefront_header', 'storefront_header_cart', 60);
+  remove_action('storefront_header', 'storefront_primary_navigation_wrapper_close', 68);
+}
+
+
+
+// add customer header
+add_action('storefront_header', 'xh_custom_header', 10);
+function xh_custom_header()
+{
+  ?>
+  <div class="xh-header-wrapper">
+    <div class="xh-header-top">
+      <div class="xh-logo">
+        <?php storefront_site_title_or_logo(); ?>
+      </div>
+      <nav class="xh-menu">
+        <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+      </nav>
+      <div class="xh-account-cart">
+        <a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))); ?>">My Account</a>
+        <a href="<?php echo esc_url(wc_get_cart_url()); ?>">Cart</a>
+      </div>
+    </div>
+    <div class="xh-header-bottom">
+      <?php get_product_search_form(); ?>
+    </div>
+  </div>
+  <?php
+}
