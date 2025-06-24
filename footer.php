db307731f782e90a6a94345ee2b0788ec67ae2a7
chouenne <134743005+chouenne@users.noreply.github.com>
2025-06-23 20:03:45 -0400

    copy footer.php widget from parent theme footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="site-footer">
+  <div class="footer-columns">
+    <?php storefront_footer_widgets(); ?>
+  </div>
+
+  <div class="footer-bottom">
+    <p>&copy; <?php echo date('Y'); ?> Digital Art Storefront. All rights reserved.</p>
+  </div>
+</footer>
